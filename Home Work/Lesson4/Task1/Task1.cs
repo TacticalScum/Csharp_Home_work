b03c02852e07ed3086bdf8b5a210f1aee2082c36
Dmitriy < ,    >
2022-04-27 20:04:01 +0700

    lesson 4 done

---
+++ Home Work/Lesson4/Task1/Task1.cs
@@ -0,0 +1,9 @@
+﻿for (int i = 0; i < 6; i++)
+{
+    int numberA = new Random().Next(0, 10);
+    int numberB = new Random().Next(0, 10);
+    Console.WriteLine("Число: " + numberA + " Степень: " + numberB);
+    Console.WriteLine("Результат: " + Convert.ToInt32(Math.Pow(numberA, numberB)));
+    Console.WriteLine();
+}
+
---
+++ Home Work/Lesson4/Task2/Task2.cs
@@ -0,0 +1,14 @@
+﻿int number = new Random().Next(0, 10000);
+int result = 0;
+Console.WriteLine("Число: " + number);
+while (true)
+{
+    int singleNum = number % 10;
+    result = result + singleNum;
+    number = number / 10;
+    if (number == 0)
+    {
+        break;
+    }
+}
+Console.WriteLine("Сумма чисел: " + result);
