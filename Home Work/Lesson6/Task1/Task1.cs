8709c4165ca36d1c36c0bdb21340b02243e910bb
Dmitriy < ,    >
2022-05-02 14:15:35 +0700

    Lesson 6 done

---
+++ Home Work/Lesson6/Task1/Task1.cs
@@ -0,0 +1,35 @@
+﻿int arrSize = 1;
+int[] array = new int[arrSize];
+string num;
+int counter = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine("Введите число или q для выхода");
+    num = Console.ReadLine();
+
+    if (num == ("q"))
+    {
+        break;
+    }
+
+    else
+    {
+        arrSize++;
+
+        array[i] = Convert.ToInt32(num);
+
+        if (i == array.Length - 1)
+        {
+            Array.Resize<int>(ref array, arrSize);
+        }
+    }
+
+    if (array[i] > 0)
+    {
+        counter++;
+    }
+}
+
+Console.WriteLine("[{0}]", string.Join(", ", array));
+Console.WriteLine("Чисел больше нуля: " + counter);
---
+++ Home Work/Lesson6/Task2/Task2.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите координату b1");
+double b1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату k1");
+double k1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату b2");
+double b2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату k2");
+double k2 = double.Parse(Console.ReadLine());
+
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны");
+}
+
+else
+{
+    double koordX = (b2 - b1) / (k1 - k2);
+    double koordY = k1 * koordX + b1;
+
+    Console.WriteLine("Координаты точки пересечения:  " + Math.Round(koordX, 3) + ", " + Math.Round(koordY, 3));
+}
