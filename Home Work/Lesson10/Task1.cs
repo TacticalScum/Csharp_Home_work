609745c34f2d7bcc0d2438a009c81b785aa31a73
Дмитрий <ya.barya@yandex.ru>
2022-05-22 18:02:56 +0700

    lesson 10 done

---
+++ Home Work/Lesson10/Task1.cs
@@ -0,0 +1,28 @@
+﻿int numbersSequenceRightBound = 0;
+
+Console.WriteLine("Введите число: ");
+numbersSequenceRightBound = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Количество групп для разбиения: {CalculateGroupsNumber(numbersSequenceRightBound)}");
+for (int i = 1; i <= CalculateGroupsNumber(numbersSequenceRightBound); i++)
+{
+    Console.WriteLine($"{i}-я группа: [{string.Join(", ", GetNextGroup(i, CalculateGroupsNumber(numbersSequenceRightBound), numbersSequenceRightBound))}]");
+}
+
+int CalculateGroupsNumber(int numbersSequenceRightBound)
+{
+    int groupsNumber = (int)Math.Ceiling(Math.Log2(numbersSequenceRightBound));
+
+    if ((int)Math.Pow(2, groupsNumber) == numbersSequenceRightBound)
+        groupsNumber++;
+
+    return groupsNumber;
+}
+
+int[] GetNextGroup(int groupNumber, int groupsQuantity, int numbersSequenceRightBound)
+{
+    if (groupNumber != groupsQuantity)
+        return Enumerable.Range((int)Math.Pow(2, groupNumber - 1), (int)Math.Pow(2, groupNumber) - (int)Math.Pow(2, groupNumber - 1)).ToArray();
+    else
+        return Enumerable.Range((int)Math.Pow(2, groupNumber - 1), numbersSequenceRightBound - (int)Math.Pow(2, groupNumber - 1) + 1).ToArray();
+}
